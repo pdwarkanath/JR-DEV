3fc55ad3774eebcc0eb170b37d4c6bc853752cf4
Dwarkanath Prabhu <dwarkanath@cmsworldwide.com>
2020-08-06 22:09:00 -0500

    api

--- client/src/App.js
+++ client/src/App.js
@@ -2,15 +2,28 @@ import React from 'react';
 import './App.css';
 import Jobs from './Jobs'
 
+const JOB_API_URL = 'http://localhost:3001/jobs'
 const mockJobs = [
   {title: 'SWe 1', company: 'Google'},
   {title: 'SWe 2', company: 'Amazon'}
 ]
 
+async function fetchJobs(updateCb){
+  const res = await fetch(JOB_API_URL);
+  const json = await res.json();
+  updateCb(json);
+  console.log({json});
+}
+
 function App() {
+  const [jobList, updateJobs] = React.useState([]);
+  React.useEffect(() => {
+    fetchJobs(updateJobs);
+  },[])
+  
   return (
     <div className="App">
-      <Jobs jobs = {mockJobs}/>
+      <Jobs jobs = {jobList}/>
     </div>
   );
 }
